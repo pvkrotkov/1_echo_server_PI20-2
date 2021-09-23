ec06647976a6a83afe9816f4b8d6dbf9a488302c
Nikita Mozharovskii <mozharovskiy02@mail.ru>
2021-09-23 20:10:14 +0300

    Практика: Эхо сервер

--- server.py
+++ server.py
@@ -1,20 +1,19 @@
 import socket
 
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
-
-msg = ''
+sock.bind(('', 9092))
 
 while True:
+	msg = ''
+	sock.listen(1)
+	conn, addr = sock.accept()
+	print(addr)
 	data = conn.recv(1024)
-	if not data:
-		break
+	#if not data:
+		#break
 	msg += data.decode()
 	conn.send(data)
-
-print(msg)
-
-conn.close()
+	print(msg)
+	if data.decode()=='exit':
+		break
+		#conn.close()
