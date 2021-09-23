a361935b21ced3c922deb562a0ebec0809a680ef
x7am <65652418+x7am@users.noreply.github.com>
2021-09-23 19:27:22 +0300

    new server commit

--- client.py
+++ client.py
@@ -3,14 +3,13 @@ from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.connect(('localhost', 9090))
 
-#msg = input()
-msg = "Hi!"
+msg =input()
+msg="End"
+
 sock.send(msg.encode())
 
 data = sock.recv(1024)
-
 sock.close()
-
 print(data.decode())
