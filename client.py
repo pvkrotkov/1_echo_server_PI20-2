84a7f2904cb8eee2e45ea5d93099420c9a2db2c8
Korneev Yaroslav <90088335+SWEETLENIN@users.noreply.github.com>
2021-09-30 19:37:49 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,12 +1,11 @@
 import socket
-from time import sleep
-
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 while True:
-    msg = input("You: ")
-    client.sendto(msg.encode('utf-8'), ('localhost', 9090))
-    if msg == 'exit!':
+    send_msg = input("Your msg: ")
+    client.sendto(send_msg.encode('utf-8'), ('localhost', 9090))
+    if send_msg == 'exit!':
         break
-    lastmsg = client.recv(1024).decode('utf-8')
-    print("Server: " + lastmsg)
-client.close()
+        back_msg = client.recv(1024).decode('utf-8')
+        print(back_msg)
+
+    client.close()
