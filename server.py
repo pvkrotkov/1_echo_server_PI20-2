79d3f6b718953ff9ed7ebc78749ef522f43ac3a8
Korneev Yaroslav <90088335+SWEETLENIN@users.noreply.github.com>
2021-09-30 19:22:06 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,22 +1,13 @@
 import socket
-
-while True:
-    sock = socket.socket()
-    sock.bind(('', 9090))
-    sock.listen(0)
-    conn, addr = sock.accept()
-    print(addr)
-
-    msg = ''
-
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        msg += data.decode()
-        conn.send(data)
-
-    print(msg)
-
-    if msg == 'exit':
-        conn.close()
+chat = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+chat.bind(('localhost', 9090))
+print('Start chat ')
+data, addr = chat.recvfrom(1024)
+    revers = data.decode('utf-8')
+    if revers == 'exit!':
+        print("Chat stopped")
+        break
+    print('User: ' + revers)
+    reply = input('Send: ')
+    chat.sendto(reply.encode('utf-8'), addr)
+chat.close()
