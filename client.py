7d777681ddc487ca3c51b8eeb1da4630559b6e6b
Andrey Kuskov <andrey.kuskov.05@gmail.com>
2021-09-30 13:33:31 +0300

    Чат с UDP

--- client.py
+++ client.py
@@ -1,26 +1,44 @@
 import socket
 from time import sleep
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('localhost', 9090))
+def addr_port():
+    addr = str(input("Введите IP-адрес компьютера: "))
+    port_name = str(input("Введите порт, по которому хотите подключиться: "))
+    if(addr == ''):
+        addr = "localhost"
+    if(port_name == ''):
+        port_name = 9090
+    return addr, int(port_name)
 
-msg = input()
-sock.send(msg.encode())
-data = sock.recv(1024)
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #sock.settimeout(0.1)
+    address, port = addr_port()
+    sock.connect((address, port))
 
-msg = input()
-sock.send(msg.encode())
-data = sock.recv(1024)
+    # msg = input()
+    # sock.send(msg.encode())
+    while True:
+        try:
+            msg = input("Введите сообщение: ")
+            sock.send(msg.encode())
+            try:
+                data = sock.recv(1024)
+                if(data.decode("UTF-8") == "exit"):
+                    print("Вы отсоедины от сервера!")
+                    break
+            except:
+                pass
+            else:
+                print("Message from: ", data.decode("UTF-8"))
+            if(msg == "exit"):
+                break
+        except KeyboardInterrupt:
+            print("Вы отсоедины от сервера!")
+            msg = "exit"
+            sock.send(msg.encode())
+            break
 
-msg = input()
-sock.send(msg.encode())
-data = sock.recv(1024)
+    sock.close()
 
-msg = input()
-sock.send(msg.encode())
-data = sock.recv(1024)
-
-sock.close()
-
-print(data.decode())
+main()
--- server.py
+++ server.py
@@ -1,19 +1,50 @@
 import socket
+import errno
+from socket import SHUT_RDWR
+import random
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-msg = ''
+def main():
+	global sock
+	users = []
+	try:
+		port = int(input("Введите порт, который хотите открыть: "))
+		sock.bind(('', port))
+	except socket.error as e:
+		if(e.errno == errno.EADDRINUSE):
+			print("Данный порт уже занят!")
+			while True:
+				try:
+					port = random.randint(0, 65535)
+					sock.bind(('', port))
+					print("Новый порт: ", port)
+					break
+				except:
+					continue
+	while True:
+		try:
+			data, conn = sock.recvfrom(1024)
+			if conn not in users:
+				users.append(conn)
+			#print(conn)
+			if(data.decode("UTF-8") == "exit"):
+				users.reverse()
+				for i in range(0, len(users)):
+					sock.sendto(b"exit", (users[i]))					
+				sock.close()
+				break
+			if(data.decode("UTF-8") == ""):
+				continue
+		except KeyboardInterrupt:
+			sock.close()
+			break
+		else:
+			print("Message: ", data.decode("UTF-8"))
+			msg = input("Введите сообщение: ")
+			sock.sendto(msg.encode(), (conn))
 
-while True:
-	data = conn.recv(1024)
-	msg = data.decode()
-	conn.send(data)
-	if(data.decode() == "exit"):
-		break
-	print(msg)
+	#print(users)
 
-conn.close()
+
+main()
