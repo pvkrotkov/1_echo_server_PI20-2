c9515f4aa73aae0c68b349624d89c874024e85f7
Иван Башмаченков <70896241+DudeBash@users.noreply.github.com>
2021-11-16 17:59:03 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,20 @@
 import socket
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+sock = socket.socket() 
+sock.bind(('', 9090)) 
+sock.listen(1) 
+conn, address = sock.accept()
 
 msg = ''
-
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+    data = conn.recv(1024)
+    if not data:
+        break
+    elif data.decode() == 'exit':
+        print("Подключение прервано")
+        break
+    msg += data.decode() 
+    conn.send(data.upper()) 
 
-print(msg)
-
+print('Полученные данные: ', msg)
 conn.close()
