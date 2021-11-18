631e8cf580e517c7bc93a085e439cf00f13c8f7a
Arina <arinadjam@gmail.com>
2021-11-18 18:55:31 +0300

    echo and chat

--- server.py
+++ server.py
@@ -2,19 +2,12 @@ import socket
 
 sock = socket.socket()
 sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
-
-msg = ''
-
+sock.listen(1)
+conn, aadr = sock.accept()
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
+    data = conn.recv(1024)
+    conn.send(data.upper())
+    if data.decode() == "exit":
+        break
 
 conn.close()
